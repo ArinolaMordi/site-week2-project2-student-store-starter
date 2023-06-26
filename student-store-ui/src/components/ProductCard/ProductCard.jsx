eaac7c70991dafdb42e9af130399e7753fcd5609
Arinola Mordi <arinola@meta.com>
2023-06-26 09:49:56 -0700

    updates being made

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -3,17 +3,17 @@ import "./ProductCard.css"
 
 
 export default function ProductCard(props) {
-    const product = props["product"]
+  const product = props["product"]
 
-    return (
-        <div className="productCard">
-            {/* Switch line 7 to an img element.Dont forget the src and alt data. */}
-            <img src={product.image} alt={product.name} />
-            <h4> {product.name} </h4>
-            <div> $ {product.price}</div>
-            <div>{product.category}</div>
+  return (
+    <div className="productCard">
 
-        </div>
-    )
+      <img src={product.image} alt={product.name} />
+      <h4> {product.name} </h4>
+      <div> $ {product.price}</div>
+      <div>{product.category}</div>
+
+    </div>
+  )
 }
 
--- student-store-ui/src/components/ProductDetails/ProductDetails.jsx
+++ student-store-ui/src/components/ProductDetails/ProductDetails.jsx
@@ -4,24 +4,23 @@ import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
 import ProductCard from "../ProductCard/ProductCard"
 export default function ProductDetails() {
-    const { id } = useParams();
-    console.log(id);
-    const [product, setProduct] = useState({});
-    //const [videoUrl, setVideoUrl] =useState("");
-    const url = `https://codepath-store-api.herokuapp.com/store/${id}`;
+  const { id } = useParams();
+  console.log(id);
+  const [product, setProduct] = useState({});
 
-    async function fetchProducts() {
-        //console.log("Fetching products")
-        const response = await fetch(url);
-        let existingData = await response.json();
-        existingData = existingData.product;
-        setProduct(existingData);
-    }
-    useEffect(() => {
-        console.log("useFetch")
-        fetchProducts();
-    }, [])
-    return (
-        <ProductCard product={product} />
-    )
+  const url = `https://codepath-store-api.herokuapp.com/store/${id}`;
+
+  async function fetchProducts() {
+    
+    const response = await fetch(url);
+    let existingData = await response.json();
+    existingData = existingData.product;
+    setProduct(existingData);
+  }
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+  return (
+    <ProductCard product={product} />
+  )
 }
